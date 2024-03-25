98a9aca006039eff00264ea9d9656960e38372d6
Domen B <42833007+luigigoesboom@users.noreply.github.com>
2024-03-26 00:46:24 +0100

    Dodal water assete, world clock
    Par water assetov za zanimirat, dodal timer ki steje sekunde od zacetka scene, label timer

--- player.gd
+++ player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
 @export var speed = 70
+@onready var world_clock = get_node("./Label")
+@onready var world_clock_time = 0
 
 func _physics_process(delta):
+	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
 	if Input.is_action_pressed("ui_right"):
@@ -15,3 +18,8 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	velocity = Vector2.ZERO
+
+
+func _on_world_clock_timeout():
+	world_clock_time += 1
+	world_clock.text = str(world_clock_time)
