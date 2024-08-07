a1e7b920a1b31c6e798528e08a3b5e12f4bbc339
Domen B. <42833007+luigigoesboom@users.noreply.github.com>
2024-08-07 17:52:20 +0200

    item pickup

---
+++ test_boss.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+enum States {
+	STATE_IDLE,
+	STATE_RANGED,
+	STATE_MELEE,
+	STATE_WAIT
+	}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
