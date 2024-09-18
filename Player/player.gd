8c6040f975e5f9e8d1288a39e8f1e360bc9ff06f
Domen B. <baumgartnerdomen@gmail.com>
2024-09-18 21:47:27 +0200

    dodal hp fp stamina bare za playerja, in mal logike za testiranje

--- Player/player.gd
+++ Player/player.gd
@@ -3,14 +3,22 @@ extends CharacterBody2D
 @export var speed = 70
 @export var jump_speed = 1800
 @export var gravity = 4000
-@export var hp : int
+@export var hp : int = 200
+@export var max_hp : int = 200
+@export var fp : int = 100
+@export var max_fp : int = 100
+@export var stam : int = 300
+@export var max_stam : int = 300
 
 @onready var animation_player = $AnimatedSprite2D
 @onready var primary_hitbox = $PrimaryAttack
 @onready var secondary_hitbox = $SecondaryAttack
 @onready var state = State.IDLE
+@onready var hp_bar = %PlayerHP
+@onready var fp_bar = %PlayerFP
+@onready var stam_bar = %PlayerSTAM
 
-enum State { IDLE, RUNNING, JUMPING, FALLING, ATTACKING_1, ATTACKING_2, ROLL }
+enum State { IDLE, RUNNING, JUMPING, FALLING, ATTACKING_1, ATTACKING_2, ROLL, DIE }
 
 signal primary_action
 signal secondary_action
@@ -23,22 +31,39 @@ var tab_inventory = load("res://Player/Inventory/TabInventory.tscn")
 var inventory_open = false
 
 func _ready():
+	hp_bar.max_value = max_hp
+	fp_bar.max_value = max_fp
+	stam_bar.max_value = max_stam
+	hp_bar.value = hp
+	fp_bar.value = fp
+	stam_bar.value = stam
 	primary_hitbox.position.x = 13.5
 	secondary_hitbox.position.x = 10.5
 	
 func _physics_process(_delta):
+	if stam < max_stam:
+		stam += 1 
+		stam_bar.value = stam
 	velocity.y += gravity * _delta
+	if hp <= 0:
+		transition_to(State.DIE)
 	if GameManager.nomove == false:
 		velocity.x = Input.get_axis("ui_left", "ui_right") * speed
 		match state:
+			State.DIE:
+				if animation_player.frame == 3:
+					animation_player.pause()
+					await get_tree().create_timer(1).timeout
+					self.visible = false
+					self.process_mode = Node.PROCESS_MODE_DISABLED
 			State.IDLE:
 				if velocity.x != 0:
 					transition_to(State.RUNNING)
 				elif !is_on_floor():
 					transition_to(State.FALLING)
-				elif Input.is_action_just_pressed("primary_action"):
+				elif Input.is_action_just_pressed("primary_action") and stam >= 25:
 					transition_to(State.ATTACKING_1)
-				elif Input.is_action_just_pressed("secondary_action"):
+				elif Input.is_action_just_pressed("secondary_action") and stam >= 45:
 					transition_to(State.ATTACKING_2)
 				elif Input.is_action_just_pressed("roll"):
 					transition_to(State.ROLL)
@@ -55,9 +80,9 @@ func _physics_process(_delta):
 					transition_to(State.IDLE)
 				if !is_on_floor():
 					transition_to(State.JUMPING)
-				elif Input.is_action_just_pressed("primary_action"):
+				elif Input.is_action_just_pressed("primary_action") and stam >= 25:
 					transition_to(State.ATTACKING_1)
-				elif Input.is_action_just_pressed("secondary_action"):
+				elif Input.is_action_just_pressed("secondary_action") and stam >= 45:
 					transition_to(State.ATTACKING_2)
 				elif Input.is_action_just_pressed("roll"):
 					transition_to(State.ROLL)
@@ -72,9 +97,9 @@ func _physics_process(_delta):
 					secondary_hitbox.position.x = -10.5
 				if is_on_floor():
 					transition_to(State.IDLE)
-				elif Input.is_action_just_pressed("primary_action"):
+				elif Input.is_action_just_pressed("primary_action") and stam >= 25:
 					transition_to(State.ATTACKING_1)
-				elif Input.is_action_just_pressed("secondary_action"):
+				elif Input.is_action_just_pressed("secondary_action") and stam >= 45:
 					transition_to(State.ATTACKING_2)
 				elif Input.is_action_just_pressed("roll"):
 					transition_to(State.ROLL)
@@ -89,9 +114,9 @@ func _physics_process(_delta):
 					secondary_hitbox.position.x = -10.5
 				if is_on_floor():
 					transition_to(State.IDLE)
-				elif Input.is_action_just_pressed("primary_action"):
+				elif Input.is_action_just_pressed("primary_action") and stam >= 25:
 					transition_to(State.ATTACKING_1)
-				elif Input.is_action_just_pressed("secondary_action"):
+				elif Input.is_action_just_pressed("secondary_action") and stam >= 45:
 					transition_to(State.ATTACKING_2)
 				elif Input.is_action_just_pressed("roll"):
 					transition_to(State.ROLL)
@@ -99,6 +124,8 @@ func _physics_process(_delta):
 				if Input.is_action_just_pressed("roll"):
 					transition_to(State.ROLL)
 				if animation_player.frame == 3:
+					stam -= 25
+					stam_bar.value = stam
 					primary_hitbox.monitoring = true
 				if animation_player.frame == 4:
 					primary_hitbox.monitoring = false
@@ -108,6 +135,8 @@ func _physics_process(_delta):
 				if Input.is_action_just_pressed("roll"):
 					transition_to(State.ROLL)
 				if animation_player.frame == 3:
+					stam -= 45
+					stam_bar.value = stam
 					secondary_hitbox.monitoring = true
 				if animation_player.frame == 9:
 					secondary_hitbox.monitoring = false
@@ -123,7 +152,7 @@ func _physics_process(_delta):
 					transition_to(State.IDLE)
 					
 	move_and_slide()
-	"""if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("interact"):
 		var dream = get_tree().get_nodes_in_group("dream")
 		for i in dream:
 			if i.visible == false:
@@ -131,17 +160,19 @@ func _physics_process(_delta):
 				i.visible = true
 			else:
 				i.process_mode = Node.PROCESS_MODE_DISABLED
-				i.visible = false"""
+				i.visible = false
 		
 	if Input.is_action_just_pressed("ui_up") and GameManager.nomove == false and is_on_floor() == true:
 		velocity.y = -jump_speed
 		transition_to(State.JUMPING)
 	
 func _process(_delta):
-	if Input.is_action_pressed("primary_action"):
+	if Input.is_action_just_pressed("primary_action") and stam >= 25:
+		hp -= 20
+		hp_bar.value = hp
 		primary_action.emit()
 		
-	if Input.is_action_pressed("secondary_action"):
+	if Input.is_action_just_pressed("secondary_action") and stam >= 45:
 		secondary_action.emit()
 	
 	#Inventory opener, spawna novga otroka UI scene
@@ -155,6 +186,8 @@ func _process(_delta):
 func transition_to(new_state):
 	state = new_state
 	match new_state:
+		State.DIE:
+			animation_player.play("die")
 		State.IDLE:
 			animation_player.play("idle")
 		State.RUNNING:
