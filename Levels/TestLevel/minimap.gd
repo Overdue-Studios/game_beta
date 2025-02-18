1a0707860cb745ca698fd7aedc7fe371f9c3c16c
andrazsovinec <andraz.sovinec@gmail.com>
2025-02-18 20:41:16 +0100

    Scuffed but functional map template
    The map now also displays the player position.

--- Levels/TestLevel/minimap.gd
+++ Levels/TestLevel/minimap.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+const SCALING_FACTOR = Vector2(0.207, 0.207)
+const OFFSETS = Vector2(220, 350.6102)
+const PLAYER_HEIGHT = Vector2(0, 35.0)
+
+const MAP_SCALE = Vector2(0.2, 0.2)
+const MAP_OFFSET = Vector2(0, 10.0)
+const PLAYER_POSITION_SCALE = Vector2(0.2, 0.5)
+
 @onready var player = GameManager.player
 @onready var player_position = $PlayerPosition
 @onready var minimap = $Map
@@ -8,20 +16,17 @@ func _ready():
 	minimap.visible = false  # Hide map on startup
 	player_position.visible = false  # Hide player position marker too
 	
-	minimap.scale = Vector2(0.18, 0.18)
+	# Scale the sprites
+	minimap.scale = MAP_SCALE
+	player_position.scale = PLAYER_POSITION_SCALE
 
 func _process(delta):
-	# Convert player's global position to map coordinates
-	# Adjust scaling_factor based on your map's size and world scale
-	var scaling_factor = Vector2(0.1, 0.1) # Example: scale down by 10%
-	player_position.position = player.global_position * scaling_factor
-	
-	#print(player.global_position)
-	
 	if minimap.visible:		
 		# Update player position marker
-		minimap.global_position = Vector2(player.global_position.x, player.global_position.y - 10)
+		minimap.global_position = player.global_position - MAP_OFFSET
+		player_position.global_position = (player.global_position - OFFSETS + PLAYER_HEIGHT) * SCALING_FACTOR + minimap.global_position
 	
 	if Input.is_action_just_pressed("OpenMap"):
-		print(minimap.position)
+		# Toggle map visibility
 		minimap.visible = !minimap.visible
+		player_position.visible = !player_position.visible
