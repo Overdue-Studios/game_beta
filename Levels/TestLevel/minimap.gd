80c9adbab0e5516c16e4152e17b8e3dd62d3fbca
andrazsovinec <andraz.sovinec@gmail.com>
2025-02-26 21:16:50 +0100

    Added some togglable settings
    Devs can now easily toggle the game pause when the map is open and player position visibility.

--- Levels/TestLevel/minimap.gd
+++ Levels/TestLevel/minimap.gd
@@ -8,11 +8,16 @@ const MAP_SCALE = Vector2(0.2, 0.2)
 const MAP_OFFSET = Vector2(0, 10.0)
 const PLAYER_POSITION_SCALE = Vector2(0.2, 0.5)
 
+const SHOW_PLAYER_POS = false
+const PAUSE_WHILE_OPEN = true
+
 @onready var player = GameManager.player
 @onready var player_position = $PlayerPosition
 @onready var minimap = $Map
 
 func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
 	minimap.visible = false  # Hide map on startup
 	player_position.visible = false  # Hide player position marker too
 	
@@ -24,9 +29,13 @@ func _process(delta):
 	if minimap.visible:		
 		# Update player position marker
 		minimap.global_position = player.global_position - MAP_OFFSET
-		player_position.global_position = (player.global_position - OFFSETS + PLAYER_HEIGHT) * SCALING_FACTOR + minimap.global_position
+		if SHOW_PLAYER_POS:
+			player_position.global_position = (player.global_position - OFFSETS + PLAYER_HEIGHT) * SCALING_FACTOR + minimap.global_position
 	
-	if Input.is_action_just_pressed("OpenMap"):
+	if Input.is_action_just_pressed("OpenMap"): 
 		# Toggle map visibility
 		minimap.visible = !minimap.visible
-		player_position.visible = !player_position.visible
+		if PAUSE_WHILE_OPEN:
+			get_tree().paused = !get_tree().paused
+		if SHOW_PLAYER_POS:
+			player_position.visible = !player_position.visible
